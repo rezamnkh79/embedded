3efe1cbeefc1384b8c93f11eb1414e3a4170b84d
Amin Chiniforoushan <48310648+aminchini@users.noreply.github.com>
2022-07-02 22:33:22 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Row, Col } from 'react-grid-system';
 import  Home from './component/Home'
 import Time from './component/time';
 
